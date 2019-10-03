ab685e3aee76ff18c89dd434bcd0febb642f0e4d
Владимир Солдатов <sold.94@mail.ru>
2019-10-03 23:08:48 +0700

    Add TODO

--- Sokoban/Sokoban/Model/GameObjects/GameObject.cs
+++ Sokoban/Sokoban/Model/GameObjects/GameObject.cs
@@ -4,6 +4,7 @@ namespace Sokoban.Model
 {
     public abstract class GameObject
     {
+        //TODO: Add TextureId Property
         public Point Coordinates { get; protected set; }
 
         protected GameObject(Point coords)
